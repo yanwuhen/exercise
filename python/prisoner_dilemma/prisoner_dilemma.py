7f97a6884611f42b9c893e3ffd3ef22dbf5d8ad7
yanwuhen51 <yanwuhen51@gmail.com>
2016-07-21 02:33:11 +0800

    完善四种类型

--- python/prisoner_dilemma/prisoner_dilemma.py
+++ python/prisoner_dilemma/prisoner_dilemma.py
@@ -2,44 +2,56 @@ import random
 
 
 class Prisoner(object):
-    type_table = {"silence": 0,
-                  "betray": 1,
-                  "flexible": 2,
-                  "revenge": 3,}
 
     def __init__(self, index, tactics, life=100):
         self.index = index
         self.tactics = tactics
         self.life = life
-        self.cooperation = None
         self.history = [0, 0, 0]
         self.betray_list = []
+        if tactics in (0, 1):
+            self.cooperation = tactics
+        else:
+            self.cooperation = 0
 
     def __str__(self):
+        type_table = {0: "silence",
+                      1: "betray",
+                      2: "flexible",
+                      3: "revenge",}
         return ("index:%d, type:%s, cooperation:%s, life:%d" %
-                (self.index, self.type, 'betay' if self.cooperation == 1 else 'silence', self.life))
+                (self.index, type_table[self.tactics], self.cooperation, self.life))
 
-    def get_cooperation(self, another):
-        if self.tactics == "flexible":
-            self.cooperation = reduce(lambda x,y: x + y, self.history) / 2
+    def get_new_cooperation(self, another):
+        if self.tactics == 2:
+            #print self.history
+            new_cooperation = reduce(lambda x,y: x + y, self.history) / 2
+            #print "start:",self.index,self.history
             self.history.pop(0)
             self.history.append(another.cooperation)
-        elif self.tactics == "revenge":
-            self.cooperation = 1 if another in self.betray_list else 0
+            #print another.cooperation
+            #print another
+            #print "end:", self.index,self.history
+        elif self.tactics == 3:
+            new_cooperation = 1 if another in self.betray_list else 0
         else:
-            self.cooperation = Prisoner.type_table[self.tactics]
+            new_cooperation = self.tactics
+        return new_cooperation
 
 
-
-    def test(self, another):
+    @staticmethod
+    def test(one, another):
+        one_new_coop = one.get_new_cooperation(another)
+        another_new_coop = another.get_new_cooperation(one)
+        one.cooperation = one_new_coop
+        another.cooperation = another_new_coop
         # 0:silence 1:betray
         table = {00: (8, 8),
                  01: (10,  -10),
                  10: (-10, 10),
                  11: (-8, -8)}
-        self.get_cooperation(another)
-        test_result = table[self.cooperation * 10 + another.cooperation]
-        self.life += test_result[0]
+        test_result = table[one.cooperation * 10 + another.cooperation]
+        one.life += test_result[0]
         another.life += test_result[1]
 
     def is_dead(self):
@@ -59,20 +71,23 @@ def start(test_list):
     length = len(test_list)
     test_order = random.sample(range(length),length)
     for one, another in zip(test_order[0::2], test_order[1::2]):
-        test_list[one].test(test_list[another])
+        Prisoner.test(test_list[one], test_list[another])
     return filter(Prisoner.is_alive, test_list)
 
 if __name__ == '__main__':
-    sum_prisoner = 10
+    sum_prisoner = 100
     test_count = 100
     prisoner_list = []
     dead_list = []
     for i in range(sum_prisoner):
-        prisoner_list.append(Prisoner(i, random.choice(range(2))))
+        prisoner_list.append(Prisoner(i, random.choice(range(4))))
     for i in range(test_count):
         prisoner_list = start(prisoner_list)
-        #silence=len(filter(Prisoner.is_silence, prisoner_list))
-        #print("silence:%d, betray:%d" % (silence, sum_prisoner - silence))
         print i
-        for p in prisoner_list:
-            print p
+        #for p in prisoner_list:
+        #    print p
+        print "sum:"
+        print "silence count:",   len(filter(lambda x: x.tactics == 0, prisoner_list))
+        print "betray count:",    len(filter(lambda x: x.tactics == 1, prisoner_list))
+        print "flexible count:",  len(filter(lambda x: x.tactics == 2, prisoner_list))
+        print "revenge count:",   len(filter(lambda x: x.tactics == 3, prisoner_list))
