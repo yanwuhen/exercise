e3cb92f25888ae0fb0bd7be85e3e13c6cb63a1ab
weberbli <weberbli@tencent.com>
2017-11-07 01:40:28 +0800

    k8m:v0.6

--- python/web_crawler/k8m.py
+++ python/web_crawler/k8m.py
@@ -3,35 +3,47 @@ import sys
 import csv
 import requests
 from bs4 import BeautifulSoup
+import bs4
 if sys.version > '3':
     import urllib.parse as parse
 else:
     import urllib as parse
+debug=True
 
-def clean_text(txt):
-    return txt
+def clean_text(contents_list):
+    ret = []
+    for node in contents_list:
+        if type(node) == bs4.element.NavigableString:
+            ret.append(node)
+        else:
+            ret.append(clean_text(node))
+    return ''.join(ret)
 
 def get_data(keyword):
     q_str = parse.urlencode({'q': keyword})
     rsp = requests.get(url='https://scholar.google.com/scholar?hl=en-US&as_sdt=0%2C5&'+ q_str)
     html_txt = rsp.text
+    if debug:
+        with open(keyword+'.html', 'w') as tmp_f:
+            tmp_f.write(html_txt.encode('utf-8'))	
     bs = BeautifulSoup(html_txt, "lxml")
     gs_ri = bs.find('div', class_='gs_ri')
     gs_rt = gs_ri.find("h3", class_='gs_rt')
-    title = gs_rt.find('a').contents[0]
+    title = gs_rt.find('a').contents
     gs_a = gs_ri.find("div", class_="gs_a")
     author_nodes = gs_a.findAll("a")
-    author = [a.contents[0] for a in author_nodes]
+    author = [clean_text(a.contents) for a in author_nodes]
+    author = ' '.join(author)
     cited_node = gs_ri.find("div", class_="gs_fl")
     cited_num = 0
     for n in cited_node.findAll("a"):
         if 'Cited by' in n.contents[0]:
             cited_num = n.contents[0][9:]
-    return clean_text(title), clean_text(author), cited_num
+    return clean_text(title), author, cited_num
 
 def calc_relate(title, line):
-    t_list = title.strip.split(' ')
-    l_line = line.strip.split(' ')
+    t_list = title.strip().split(' ')
+    l_line = line.strip().split(' ')
     cnt = 0
     reverse_cnt = 0
     for t in t_list:
@@ -48,14 +60,17 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise Exception('usage: xxx your_input_file')
     input_file = sys.argv[1]
+    output_file = 'output.csv'
     if not os.path.exists(input_file):
         raise Exception('error: file no exist.')
-    if os.path.exists('output.cvs'):
+    if os.path.exists(output_file) and not debug:
         raise Exception('error: output.cvs aleady exist.')
     else:
-        writer = csv.writer(open('output.cvs', 'w'))
+        writer = csv.writer(open(output_file, 'w'))
+        writer.writerow(['line', 'title', 'author', 'cited_num', 'relate', 'reverse_relate'])
     with open(input_file, 'r') as in_f:
         for line in in_f.readlines():
-            title, author, cited_num = get_data(line.strip())
+            line = line.strip()
+            title, author, cited_num = get_data(line)
             relate, reverse_relate = calc_relate(title, line)
             writer.writerow([line, title, author, cited_num, relate, reverse_relate])
