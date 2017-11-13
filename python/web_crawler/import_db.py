2d78ca48f7be6e18af4297e4b58d23bd892cb3a7
weberbli <weberbli@tencent.com>
2017-11-13 20:50:23 +0800

    import 2 db: add count

--- python/web_crawler/import_db.py
+++ python/web_crawler/import_db.py
@@ -5,7 +5,7 @@ import sqlite3
 conn = sqlite3.connect('k8m.db')
 cursor = conn.cursor()
 dr = csv.reader(open('output.csv', 'r', encoding='utf-8'))
-p_same = a_same = 0
+cnt = p_same = a_same = 0
 for row in dr:
 	line = row[0]
 	cursor.execute('select line,title,author,cited_num,relate,reverse_relate from k8m where line=?', [line])
@@ -16,7 +16,12 @@ for row in dr:
 			print('line is same, and other is diff: %s' % line)
 		else:
 			a_same += 1
+		continue
 	cursor.execute('insert into k8m(line,title,author,cited_num,relate,reverse_relate) values(?,?,?,?,?,?)', row)
+	conn.commit()
+	cnt += 1
+	if cnt % 1000 == 0:
+		print(cnt)
 print('p_same is %d, a_same is %d' % (p_same, a_same))
 #create sql
 #create table k8m (line VARCHAR(65536), title VARCHAR(65536), author VARCHAR(1024), cited_num VARCHAR(128), relate VARCHAR(128), reverse_relate  VARCHAR(128));create table k8m (line VARCHAR(65536), title VARCHAR(65536), author VARCHAR(1024), cited_num VARCHAR(128), relate VARCHAR(128), reverse_relate  VARCHAR(128));
