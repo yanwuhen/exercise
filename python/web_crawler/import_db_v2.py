11e85231315aab708caa817bc031cb60967a707f
weberbli <weberbli@tencent.com>
2017-11-15 03:07:05 +0800

    import xls

--- python/web_crawler/import_db_v2.py
+++ python/web_crawler/import_db_v2.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import sqlite3
+import xlrd
 
 CREATE_TABLE = 'CREATE TABLE IF NOT EXISTS k8m (id INTEGER,'\
                               ' line VARCHAR(65536),'\
@@ -21,7 +22,6 @@ conn.commit()
 
 def import_output(output_file='output.csv'):
 	dr = csv.reader(open(output_file, 'r', encoding='utf-8'))
-	cnt = p_same = a_same = 0
 	for row in dr:
 		#cursor.execute('Insert Or Replace Into k8m(line,title,author,cited_num,relate,reverse_relate) values(?,?,?,?,?,?)', row)
 		cursor.execute('update k8m set title=?, author=?, cited_num=?, relate=?, reverse_relate=? where line=?', row[1:]+ [row[0]])
@@ -44,7 +44,24 @@ def import_file(input_file='input.txt', title=''):
 			cursor.execute('insert into k8m(id, line,title,author,cited_num,relate,reverse_relate) values(?,?,?,?,?,?)', [id, ln, title, '', '', '', ''])
 			conn.commit()
 
-import_file()
+def import_xls(input_file='k8m.xls'):
+	data = xlrd.open_workbook(input_file)
+	sheets = data.sheets()
+	st = data.sheet_by_name('all publication Feb21')
+	keyword_list = st.col_values(3)
+	id_list = st.col_values(1)
+	for z in zip(id_list[3:], keyword_list[3:]):
+		id, ln = z
+		ln = ln.strip()
+		if ln is None or ln == '':
+			continue
+		ln = ln.replace('\n', ' ')
+		id = int(id)
+		cursor.execute('insert into k8m(id, line,title,author,cited_num,relate,reverse_relate) values(?,?,?,?,?,?,?)', [id, ln, '', '', '', '', ''])
+		conn.commit()
+
+import_xls()
+#import_file()
 import_output(output_file='output.csv')
 import_output(output_file='./1114/output.csv')
 update_file(input_file='./1114/fail.txt', title='fail')
