0a05c15c3a532602afbd882e29a273feb0c2c676
libin <yanwuhen51@163.com>
2020-04-09 15:50:42 +0800

    入侵检测改为保存视频

--- python/wx_dev/new_wx/intrusion_detction.py
+++ python/wx_dev/new_wx/intrusion_detction.py
@@ -1,11 +1,25 @@
 import cv2
+import time
+
+MAX_THRESHOLD = 3   #多少帧变化才录像
+MAX_DELTA_SUM = 300 #多大变化才算
+WIDTH = 640
+HEIGHT = 480
+FPS = 20
+
 cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture("test_data_360.mp4")
 if not cap.isOpened():
     print("dont open")
     exit
-# max_cap = 1000
-i = 0
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+cap.set(cv2.CAP_PROP_FPS, FPS)
+fourcc = cv2.VideoWriter_fourcc(*'AVC1')
+out = None
+
+intrusion_threshold = MAX_THRESHOLD
+i = last_change = 0
 bgray = None
 while True:
     i += 1
@@ -22,14 +36,19 @@ while True:
         ngray), cv2.convertScaleAbs(bgray.copy()))
     bgray = ngray
     thresh = cv2.threshold(frameDelta, 45, 255, cv2.THRESH_BINARY)[1]
-    # if i == 450:
-    #     cv2.imwrite("0409_%d_delta.jpg" % i, frameDelta)
-    #     cv2.imwrite("0409_%d_old.jpg" % i, bgray)
-    #     cv2.imwrite("0409_%d_new.jpg" % i, ngray)
-    if thresh.sum() > 300:
+    if thresh.sum() > MAX_DELTA_SUM:
         print(i, thresh.sum())
-        cv2.imwrite("jk/%d_new.jpg" % i, ngray)
-        # max_cap -= 1
-        # if max_cap <= 0:
-        #     break
+        #cv2.imwrite("/mnt/jk/%d_new.jpg" % i, ngray)
+        if i == last_change + 1:
+            intrusion_threshold -= 1
+        last_change = i
+    if intrusion_threshold < 0 and out is None:
+        out = cv2.VideoWriter('/mnt/jk/save-%s.avi' % time.strftime('%y-%m-%d-%H-%M-%S'), fourcc, FPS, (WIDTH, HEIGHT))
+    if last_change != i:
+        intrusion_threshold = MAX_THRESHOLD
+        if out is not None:
+            out.release()
+            out = None
+    if out is not None:
+        out.write(nframe)
 cap.release()
